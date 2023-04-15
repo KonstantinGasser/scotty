5ba2906eb0955b489a20f6930c042e5c6bf1e571
Konstantin Gasser <konstantin.gasser@me.com>
2023-04-15 20:21:31 +0200

    allow pause and continue while talling logs

--- app/component/tailing/bindings.go
+++ app/component/tailing/bindings.go
@@ -1,1 +1,13 @@
 package tailing
+
+import "github.com/charmbracelet/bubbles/key"
+
+type bindings struct {
+	Pause key.Binding
+}
+
+var defaultBindings = bindings{
+	Pause: key.NewBinding(
+		key.WithKeys("p"),
+	),
+}
--- app/component/tailing/tailing.go
+++ app/component/tailing/tailing.go
@@ -6,23 +6,32 @@ import (
 	"github.com/KonstantinGasser/scotty/debug"
 	"github.com/KonstantinGasser/scotty/multiplexer"
 	"github.com/KonstantinGasser/scotty/store"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 const (
 	borderMargin = 1
+
+	unset = iota
+	running
+	paused
 )
 
 type Model struct {
 	ready         bool
 	width, height int
 	pager         store.Pager
+	state         int
+	bindings      bindings
 }
 
 func New(pager store.Pager) *Model {
 	return &Model{
-		ready: false,
-		pager: pager,
+		ready:    false,
+		pager:    pager,
+		state:    unset,
+		bindings: defaultBindings,
 	}
 }
 
@@ -36,23 +45,23 @@ func (model *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	)
 
 	switch msg := msg.(type) {
-	case event.DimensionMsg:
-		model.setDimensions(
-			msg.AvailableWidth,
-			msg.AvailableHeight,
-		)
 
-		if !model.ready {
-			model.pager.Reset(model.width, uint8(model.height))
-			model.ready = true
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, model.bindings.Pause):
+			if model.state == paused {
+				model.state = running
+				break
+			}
+			model.state = paused
 		}
-
 	case tea.WindowSizeMsg:
 		model.setDimensions(msg.Width, msg.Height)
 
 		if !model.ready {
 			model.pager.Reset(model.width, uint8(model.height))
 			model.ready = true
+			model.state = running
 			break
 		}
 
@@ -68,6 +77,9 @@ func (model *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		debug.Print("not implemented yet!")
 
 	case multiplexer.Message:
+		if model.state == paused {
+			break
+		}
 		model.pager.MoveDown()
 	}
 
