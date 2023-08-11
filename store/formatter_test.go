0e4faf38e4ff14c48d373adcdc11884cd959a9da
Konstantin Gasser <konstantin.gasser@me.com>
2023-08-11 09:08:37 +0200

    adding test to ensure resize of format buffer is working

--- store/formatter_test.go
+++ store/formatter_test.go
@@ -30,14 +30,14 @@ func TestBuildView(t *testing.T) {
 	formatted := formatter.String()
 
 	want := []string{
-		`[1]>╭────────────────────────────────────────╮..`,
-		`[2]t│ test |                                 │..`,
-		`[3]t│ {                                      │..`,
-		`[4]t│   "hello": "world",                    │..`,
-		`[5]t│   "index": 1,                          │..`,
-		`[6]t│   "level": "debug"                     │..`,
-		`[7]t│ }                                      │..`,
-		`[8]t╰────────────────────────────────────────╯..`,
+		`>>>t╭────────────────────────────────────────╮..`,
+		`test│ test |                                 │..`,
+		`test│ {                                      │..`,
+		`test│   "hello": "world",                    │..`,
+		`test│   "index": 1,                          │..`,
+		`test│   "level": "debug"                     │..`,
+		`test│ }                                      │..`,
+		`test╰────────────────────────────────────────╯..`,
 	}
 
 	for i, line := range strings.Split(formatted, "\n") {
@@ -54,4 +54,74 @@ THANKS!
 <----BEGIN---->
 %s
 <----END---->`, formatted)
+}
+
+func TestBuildViewAfterResize(t *testing.T) {
+	capacity := 62
+	fill := 32
+	beforePageSize := 16
+	afterPagerSize := 8
+	ttyWidth := 50
+
+	store := New(uint32(capacity))
+
+	formatter := store.NewFormatter(uint8(beforePageSize), ttyWidth)
+
+	testLabel := "test"
+	testLog := `{"hello": "world", "level": "debug", "index": {index}}`
+	for i := 0; i < fill; i++ {
+		log := strings.Replace(testLog, "{index}", fmt.Sprint(i), 1)
+		raw := testLabel + " | " + log
+
+		store.Insert(testLabel, len(testLabel+" | "), []byte(raw))
+	}
+
+	formatter.Load(1)
+
+	before := formatter.String()
+	wantBefore := []string{
+		`>>>test | {"hello": "world", "level": "debug"...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test╭────────────────────────────────────────╮..`,
+		`test│ test |                                 │..`,
+		`test│ {                                      │..`,
+		`test│   "hello": "world",                    │..`,
+		`test│   "index": 1,                          │..`,
+		`test│   "level": "debug"                     │..`,
+		`test│ }                                      │..`,
+		`test╰────────────────────────────────────────╯..`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+		`test | {"hello": "world", "level": "debug", "...`,
+	}
+
+	for i, line := range strings.Split(before, "\n") {
+		if wantBefore[i] != line {
+			t.Fatalf("[before-resize] wanted line: %q - got: %q", wantBefore[i], line)
+		}
+	}
+
+	formatter.Resize(ttyWidth, uint8(afterPagerSize))
+
+	after := formatter.String()
+
+	wantAfter := []string{
+		`>>>t╭────────────────────────────────────────╮..`,
+		`test│ test |                                 │..`,
+		`test│ {                                      │..`,
+		`test│   "hello": "world",                    │..`,
+		`test│   "index": 1,                          │..`,
+		`test│   "level": "debug"                     │..`,
+		`test│ }                                      │..`,
+		`test╰────────────────────────────────────────╯..`,
+	}
+
+	for i, line := range strings.Split(after, "\n") {
+		if wantAfter[i] != line {
+			t.Fatalf("[after-resize] wanted line: %q - got: %q", wantAfter[i], line)
+		}
+	}
 }
