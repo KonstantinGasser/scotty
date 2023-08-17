a67be9191845a673b903c8da7cf08f5c1f334e32
Konstantin Gasser <konstantin.gasser@me.com>
2023-08-17 19:20:01 +0200

    commented test cases

--- store/line_manipulation_test.go
+++ store/line_manipulation_test.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/KonstantinGasser/scotty/store/ring"
@@ -46,70 +45,64 @@ func TestLineWrap(t *testing.T) {
 				`            | stacktrace":"main.handleLog\n\t/Users/konstantingasser/coffecode/scotty/test/application/structred.go:68\nmain.main\n\t/Users/konstantingasser/coffecode/scotty/ap`,
 				`            | plication/structred.go:47\nruntime.main\n\t/usr/local/go/src/runtime/proc.go:250"}`,
 			},
-			// want: []string{
-			// 	`hello-world | {"level":"error","ts":1692212915.723973,"caller":"application/structred.go:68","msg":"unable to do X","index":52,"error":"unable to do X","ts":1692212915.723955,"`,
-			// 	`stacktrace":"main.handleLog\n\t/Users/konstantingasser/coffecode/scotty/test/application/structred.go:68\nmain.main\n\t/Users/konstantingasser/coffecode/scotty/application/str`,
-			// 	`uctred.go:47\nruntime.main\n\t/usr/local/go/src/runtime/proc.go:250"}`,
-			// },
 		},
-		// {
-		// 	name:     "short log line",
-		// 	ttyWidth: 45,
-		// 	item:     makeItem(bodyShort),
-		// 	want: []string{
-		// 		`hello-world | time="2023-08-16T19:06:36+02:00`,
-		// 		`            | " level=error msg="msg=unable t`,
-		// 		`            | o do X, error=unable to do X, index=7"`,
-		// 	},
-		// },
-		// {
-		// 	name:     "medium log line",
-		// 	ttyWidth: 65,
-		// 	item:     makeItem(bodyMedium),
-		// 	want: []string{
-		// 		`hello-world | {"level":"error","ts":1692205600.785263,"caller":"a`,
-		// 		`            | pplication/structred.go:68","msg":"unable to do X",`,
-		// 		`            | "index":16,"error":"unable to do X","ts":1692205600`,
-		// 		`            | .785238,"stacktrace":"main.handleLog\n\t/Users/kons`,
-		// 		`            | tantingasser/coffecode/scotty/test/application/stru`,
-		// 		`            | ctred.go:68\nmain.main\n\t/Users/konstantingasser/c`,
-		// 		`            | offecode/scotty/test/application/structred.go:47\nr`,
-		// 		`            | untime.main\n\t/usr/local/go/src/runtime/proc.go:250"}`,
-		// 	},
-		// },
-		// {
-		// 	name:     "long log line",
-		// 	ttyWidth: 100,
-		// 	item:     makeItem(bodyLong),
-		// 	want: []string{
-		// 		`hello-world | {"insertId":"42","jsonPayload":{"message":"There was an error in the application","tim`,
-		// 		`            | es":"2019-10-12T07:20:50.52Z"},"httpRequest":{"requestMethod":"GET"},"resource":{"type`,
-		// 		`            | ":"k8s_container","labels":{"container_name":"hello-app","pod_name":"helloworld-gke-6c`,
-		// 		`            | fd6f4599-9wff8","project_id":"stackdriver-sandbox-92334288","namespace_name":"default"`,
-		// 		`            | ,"location":"us-west4","cluster_name":"helloworld-gke"}},"timestamp":"2020-11-07T15:57`,
-		// 		`            | :35.945508391Z","severity":"ERROR","labels":{"user_label_2":"value_2","user_label_1":"`,
-		// 		`            | value_1"},"logName":"projects/stackdriver-sandbox-92334288/logs/stdout","operation":{"`,
-		// 		`            | id":"get_data","producer":"github.com/MyProject/MyApplication","first":true},"trace":"`,
-		// 		`            | projects/my-projectid/traces/06796866738c859f2f19b7cfb3214824","sourceLocation":{"file`,
-		// 		`            | ":"get_data.py","line":"142","function":"getData"},"receiveTimestamp":"2020-11-07T15:5`,
-		// 		`            | 7:42.411414059Z","spanId":"000000000000004a"}`,
-		// 	},
-		// },
+		{
+			name:     "short log line",
+			ttyWidth: 45,
+			item:     makeItem(bodyShort),
+			want: []string{
+				`hello-world | time="2023-08-16T19:06:36+02:00`,
+				`            | " level=error msg="msg=unable t`,
+				`            | o do X, error=unable to do X, index=7"`,
+			},
+		},
+		{
+			name:     "medium log line",
+			ttyWidth: 65,
+			item:     makeItem(bodyMedium),
+			want: []string{
+				`hello-world | {"level":"error","ts":1692205600.785263,"caller":"a`,
+				`            | pplication/structred.go:68","msg":"unable to do X",`,
+				`            | "index":16,"error":"unable to do X","ts":1692205600`,
+				`            | .785238,"stacktrace":"main.handleLog\n\t/Users/kons`,
+				`            | tantingasser/coffecode/scotty/test/application/stru`,
+				`            | ctred.go:68\nmain.main\n\t/Users/konstantingasser/c`,
+				`            | offecode/scotty/test/application/structred.go:47\nr`,
+				`            | untime.main\n\t/usr/local/go/src/runtime/proc.go:250"}`,
+			},
+		},
+		{
+			name:     "long log line",
+			ttyWidth: 100,
+			item:     makeItem(bodyLong),
+			want: []string{
+				`hello-world | {"insertId":"42","jsonPayload":{"message":"There was an error in the application","tim`,
+				`            | es":"2019-10-12T07:20:50.52Z"},"httpRequest":{"requestMethod":"GET"},"resource":{"type`,
+				`            | ":"k8s_container","labels":{"container_name":"hello-app","pod_name":"helloworld-gke-6c`,
+				`            | fd6f4599-9wff8","project_id":"stackdriver-sandbox-92334288","namespace_name":"default"`,
+				`            | ,"location":"us-west4","cluster_name":"helloworld-gke"}},"timestamp":"2020-11-07T15:57`,
+				`            | :35.945508391Z","severity":"ERROR","labels":{"user_label_2":"value_2","user_label_1":"`,
+				`            | value_1"},"logName":"projects/stackdriver-sandbox-92334288/logs/stdout","operation":{"`,
+				`            | id":"get_data","producer":"github.com/MyProject/MyApplication","first":true},"trace":"`,
+				`            | projects/my-projectid/traces/06796866738c859f2f19b7cfb3214824","sourceLocation":{"file`,
+				`            | ":"get_data.py","line":"142","function":"getData"},"receiveTimestamp":"2020-11-07T15:5`,
+				`            | 7:42.411414059Z","spanId":"000000000000004a"}`,
+			},
+		},
 	}
 
 	for _, tc := range tt {
 		lines := lineWrap(tc.item, tc.ttyWidth)
 
-		t.Logf("\n%s", strings.Join(lines, "\n"))
-		// if len(lines) != len(tc.want) {
-		// 	t.Fatalf("[%s] number of lines do not match.\n\tWanted: %d\n\tGot: %d", tc.name, len(tc.want), len(lines))
-		// }
-		//
-		// for i, line := range lines {
-		// 	if line != tc.want[i] {
-		// 		t.Fatalf("[%s] lines do not match.\n\tWanted: %s\n\tGot: %s", tc.name, tc.want[i], line)
-		// 	}
-		// }
+		if len(lines) != len(tc.want) {
+			t.Fatalf("[%s] number of lines do not match.\n\tWanted: %d\n\tGot: %d", tc.name, len(tc.want), len(lines))
+		}
+
+		for i, line := range lines {
+			if line != tc.want[i] {
+				t.Fatalf("[%s] lines do not match.\n\tWanted: %s\n\tGot: %s", tc.name, tc.want[i], line)
+			}
+		}
 
 	}
 }
